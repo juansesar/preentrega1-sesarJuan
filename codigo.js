d39127d0b64ff472f0c658a6bcc732fcdd266f29
JuanSesar <juanmsesar@gmail.com>
2023-07-10 07:51:09 -0300

    filterfunciona

--- codigo.js
+++ codigo.js
@@ -7,116 +7,69 @@ class Producto {
     }
 }
 
-// 
+function buscarProducto(array, elemento){
+    let busqueda= ""
+    busqueda = array.filter(
+        (p) => p.producto == elemento 
+     )
+    if (array.length == 0 ){
+        alert("este producto no existe")
+    }else{
+        console.log(busqueda)
+        mostrarProducto(busqueda)
+           
+    }
+}
 
-// function validarDatos(num){
-//     while(isNaN(num)){
-//         num= parseInt(prompt('el valor ingresado no es numerico, vuelva a intentarlo'));
-//     }
-//     return num
-// }
-// function minimoDias(x){
-//     while(x<30){
-//         x= parseInt(prompt(`la cantidad de dias minima es de 30, vuelva a ingresar la cantidad de dias`));
-//         break
-//     }
-//     return x 
-// }
+function mostrarProducto(array){
+    cardsdiv.innerHTML = ""
+    for( let p of array){
+        let nuevoProducto= document.createElement("div")
+        nuevoProducto.innerHTML = `<div id="${p.id}" class="card" style="width: 18rem;">
+                                <img src="${p.imagen}" class="card-img-top" height=300px alt="">
+                                <div class="card-body">
+                                    <h5 class="card-title">${p.producto}</h5>
+                                    <p class="card-text">${p.precio}.</p>
+                                    <a href="#" class="btn btn-primary">comprar</a>
+                                </div>
+                                </div>`
+        cardsdiv.appendChild(nuevoProducto)
+   
+    }
+}
 
-// function calculo (inversion, dias, intereses){
-//     let tasa= (intereses/365)*dias;
-//     return  inversion+((inversion*tasa)/100);
-// }
+let cardsdiv = document.getElementById("cards")
+let mostrarCatalogo = document.getElementById("catalogo")
+let botonProductoRemeras = document.getElementById("btnRemeras")
+let botonProductoPosters = document.getElementById("btnposters")
+let botonProductoAccesorios = document.getElementById("btnAccesorios")
+let botonCarrito = document.getElementById("carrito")
 
 let producto1= new Producto("1", "Remera estampada", 3000 , "./imagenes/remera1.png")
 let producto2= new Producto("2", "Remera estampada", 4000 , "./imagenes/remera2.jpg")
-let producto3= new Producto("3", "poster", 1000 , "./imagenes/poster1.jpg")
-let producto4= new Producto("4", "poster", 1500 , "./imagenes/poster2.jpg")
-let producto5= new Producto("5", "moneda", 4000 , "./imagenes/moneda.jpg")
-let producto6= new Producto("6", "termo", 3000 , "./imagenes/termo.jpg")
+let producto3= new Producto("3", "Poster", 1000 , "./imagenes/poster1.jpg")
+let producto4= new Producto("4", "Poster", 1500 , "./imagenes/poster2.jpg")
+let producto5= new Producto("5", "Accesorios", 4000 , "./imagenes/moneda.jpg")
+let producto6= new Producto("6", "Accesorios", 3000 , "./imagenes/termo.jpg")
+
+// localStorage.setItem()
 
 const productos=[]
 
 productos.push(producto1, producto2, producto3, producto4, producto5, producto6)
 
-let cardsdiv = document.getElementById("cards")
+mostrarCatalogo.addEventListener("click", (e) => {
+    e.preventDefault()
+    mostrarProducto(productos)})
 
-
-for( let p of productos){
-    let nuevoProducto= document.createElement("div")
-    nuevoProducto.innerHTML = `<div id="${p.id}" class="card" style="width: 18rem;">
-                            <img src="${p.imagen}" class="card-img-top" alt="">
-                            <div class="card-body">
-                                <h5 class="card-title">${p.producto}</h5>
-                                <p class="card-text">${p.precio}.</p>
-                                <a href="#" class="btn btn-primary">Agregar al carrito</a>
-                            </div>
-                            </div>`
-    cardsdiv.appendChild(nuevoProducto)
-
-}
-
-
-console.log (productos)
-
-let botonProductoRemeras = document.getElementById("btnRemeras")
-let botonProductoPosters = document.getElementById("btnPosters")
-let botonProductoAccesorios = document.getElementById("btnAccesorios")
-
-
-function buscarProducto(array, elemento){
-         
-        let busqueda = array.filter(
-            (dato) => dato.producto == elemento 
-         )
-        if (busqueda == undefined ){
-            alert("este producto no existe")
-        }else{
-            console.log(busqueda)
-            
-         }
-    }
-
-
-  
-botonProductoRemeras.onclick= () => {buscarProducto(productos,"Remera estampada")}
-botonProductoPosters.onclick= () => {buscarProducto(productos, "Posters")}  
-botonProductoAccesorios.onclick= () => {buscarProducto(productos,"Accesorios") }
-// let inversion= parseInt( prompt('ingrese el monto que desea invertir'))
-// let inversionV= validarDatos(inversion)
-// if(inversionV != 0 || inversion <= 0 ){
-//     if (inversion <= 0){
-//         while(true){
-//             inversion= parseInt( prompt('El Monto ingresado tiene que ser mayor a 0, por favor ingrese el monto que desea invertir'))
-//             break
-//         }
-//     }
-//     else if (inversionV != 0){
-//         inversion= inversionV
-//     }
-
-// }
-
-// let dias= parseInt(prompt('ingrese la cantidad de dias del plazo fijo'))
-// let diasV= validarDatos(dias)
-// if (diasV != 0){
-//     dias=diasV
-// }
-// let diasM=minimoDias(dias)
-// if (diasM != 0){
-//     dias=diasM
-// }
-// let intereses= parseInt(75)
-// let respuesta= calculo(inversion, dias, intereses)
-// let parcialx=0
-
-// for ( let i= 1; i <= 12; i++ ){  
-    
-//     let tasa = intereses/12
-//     let respuesta2= (inversion*tasa)/100
-//     parcialx= parcialx + respuesta2
-    
-//  }
-//  total = inversion + parcialx
-
-// alert(`el rendimiento de su inversion de ${inversion} pesos, en un plazo de ${dias} dias, es de ${respuesta}, el interes compuesto en un plazo de 12 meses es de ${total}`)
+botonProductoRemeras.addEventListener("click", (e) => {
+    e.preventDefault()
+    buscarProducto( productos,"Remera estampada")})
+botonProductoAccesorios.addEventListener("click", (e) => {
+    e.preventDefault()
+    buscarProducto( productos,"Accesorios")
+})  
+botonProductoPosters.addEventListener("click", (e) => {
+    e.preventDefault()
+    buscarProducto( productos, "Poster")
+})
